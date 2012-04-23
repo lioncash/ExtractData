15fd6737886cd7b2f0fc250a75f0c2d01799688d
lioncash <mathew1900@hotmail.com>
2012-04-22 20:50:11 -0400

    - Fix the options menu. (Everything is now completely readable)
    - Move solution over to visual studio 2010
    
    - Add zlib and libpng to the source tree for easier development.
    
    Add readme in the new libs directory so you know how to compile much better.

--- ArcFile.cpp
+++ ArcFile.cpp
@@ -1,4 +1,3 @@
-
 #include	"stdafx.h"
 #include	"Common.h"
 #include	"Dialog/ExistsDialog.h"
--- Ctrl/ListView.h
+++ Ctrl/ListView.h
@@ -10,49 +10,49 @@
 
 // When you sort the list of parameters
 struct SORTPARAM {
-	int column;
-	BOOL direct; // 0 = ascending order, 1 = descending order
+    int column;
+    BOOL direct; // 0 = ascending order, 1 = descending order
 };
 
 class CListView {
 private:
-	HIMAGELIST m_hImage;
-	UINT m_uID;
+    HIMAGELIST m_hImage;
+    UINT m_uID;
 
 protected:
-	HWND m_hWnd;
-	HWND m_hList;
-	HINSTANCE m_hInst;
-	SOption* m_pOption;
-	SORTPARAM m_sort;
-	static SORTPARAM* m_pSort;
+    HWND m_hWnd;
+    HWND m_hList;
+    HINSTANCE m_hInst;
+    SOption* m_pOption;
+    SORTPARAM m_sort;
+    static SORTPARAM* m_pSort;
 
-	HWND Create(UINT uID, std::vector<LVCOLUMN>& lvcols, int x = 0, int y = 0, int cx = 0, int cy = 0);
-	template <typename TYPE> static BOOL retCompare(TYPE a, TYPE b) { return ((m_pSort->direct) ? (a > b) : (a < b)); }
-	virtual void OnSort() {}
+    HWND Create(UINT uID, std::vector<LVCOLUMN>& lvcols, int x = 0, int y = 0, int cx = 0, int cy = 0);
+    template <typename TYPE> static BOOL retCompare(TYPE a, TYPE b) { return ((m_pSort->direct) ? (a > b) : (a < b)); }
+    virtual void OnSort() {}
 
 public:
-	CListView();
-	virtual ~CListView();
+    CListView();
+    virtual ~CListView();
 
-	void Init(HWND hWnd, SOption& option);
-	void SaveIni();
-	void Close();
+    void Init(HWND hWnd, SOption& option);
+    void SaveIni();
+    void Close();
 
-	void SetBkColor();
-	void SetTextColor();
+    void SetBkColor();
+    void SetTextColor();
 
-	void Sort(int column);
-	void Enable(BOOL flag);
-	void SetItemSelAll(UINT flag) { ListView_SetItemState(m_hList, -1, flag, LVIS_SELECTED); }
-	void SetFocus() { ::SetFocus(m_hList); }
-	void SetWindowPos(int x, int y, int cx, int cy) { MoveWindow(m_hList, x, y, cx, cy, TRUE); }
-	void Update();
+    void Sort(int column);
+    void Enable(BOOL flag);
+    void SetItemSelAll(UINT flag) { ListView_SetItemState(m_hList, -1, flag, LVIS_SELECTED); }
+    void SetFocus() { ::SetFocus(m_hList); }
+    void SetWindowPos(int x, int y, int cx, int cy) { MoveWindow(m_hList, x, y, cx, cy, TRUE); }
+    void Update();
 
-	UINT	GetCountSel()	{ return (ListView_GetSelectedCount(m_hList)); }
-	INT		GetCount()		{ return (ListView_GetItemCount(m_hList)); }
-	HWND	GetHandle()		{ return (m_hList); }
+    UINT	GetCountSel()	{ return (ListView_GetSelectedCount(m_hList)); }
+    INT		GetCount()		{ return (ListView_GetItemCount(m_hList)); }
+    HWND	GetHandle()		{ return (m_hList); }
 
-	int	GetNextItem(int nItem) { return ListView_GetNextItem(m_hList, nItem, LVNI_ALL | LVNI_SELECTED); }
-	int GetFocusItem()	{ return ListView_GetNextItem(m_hList, -1, LVNI_ALL | LVNI_FOCUSED); }
+    int	GetNextItem(int nItem) { return ListView_GetNextItem(m_hList, nItem, LVNI_ALL | LVNI_SELECTED); }
+    int GetFocusItem()	{ return ListView_GetNextItem(m_hList, -1, LVNI_ALL | LVNI_FOCUSED); }
 };
