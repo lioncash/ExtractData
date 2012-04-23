e257e390d9834eb9c1111cd560d7dce48e4b6d83
lioncash <mathew1900@hotmail.com>
2012-04-23 13:47:20 -0400

    More styling

--- Dialog/FolderDialog.cpp
+++ Dialog/FolderDialog.cpp
@@ -4,39 +4,41 @@
 
 BOOL CFolderDialog::DoModal(HWND hWnd, LPCTSTR lpszTitle, LPTSTR pDir)
 {
-	BROWSEINFO bi;
-	ZeroMemory(&bi, sizeof(BROWSEINFO));
-	bi.hwndOwner = hWnd;
-	bi.lpfn = (BFFCALLBACK)BrowseCallBackProc;
-	bi.ulFlags = BIF_RETURNONLYFSDIRS | BIF_USENEWUI;
-	bi.lParam = (LPARAM)pDir;
-	bi.lpszTitle = lpszTitle;
+    BROWSEINFO bi;
+    ZeroMemory(&bi, sizeof(BROWSEINFO));
+    bi.hwndOwner = hWnd;
+    bi.lpfn = (BFFCALLBACK)BrowseCallBackProc;
+    bi.ulFlags = BIF_RETURNONLYFSDIRS | BIF_USENEWUI;
+    bi.lParam = (LPARAM)pDir;
+    bi.lpszTitle = lpszTitle;
 
-	LPITEMIDLIST pItemID = SHBrowseForFolder(&bi);
-	if (pItemID == NULL)
-		return FALSE;
+    LPITEMIDLIST pItemID = SHBrowseForFolder(&bi);
+    if (pItemID == NULL)
+        return FALSE;
 
-	LPMALLOC pMalloc = NULL;
-	if (SHGetMalloc(&pMalloc) == E_FAIL) {
-		CError error;
-		error.Message(hWnd, _T("SHGetMalloc Error"));
-		return FALSE;
-	}
+    LPMALLOC pMalloc = NULL;
+    if (SHGetMalloc(&pMalloc) == E_FAIL)
+    {
+        CError error;
+        error.Message(hWnd, _T("SHGetMalloc Error"));
+        return FALSE;
+    }
 
-	SHGetPathFromIDList(pItemID, pDir);
+    SHGetPathFromIDList(pItemID, pDir);
 
-	pMalloc->Free(pItemID);
-	pMalloc->Release();
+    pMalloc->Free(pItemID);
+    pMalloc->Release();
 
-	return TRUE;
+    return TRUE;
 }
 
 LRESULT CALLBACK CFolderDialog::BrowseCallBackProc(HWND hWnd, UINT uMsg, LPARAM lParam, LPARAM lpData)
 {
-	switch (uMsg) {
-		case BFFM_INITIALIZED:
-			SendMessage(hWnd, BFFM_SETSELECTION, (WPARAM)TRUE, lpData);
-			break;
-	}
-	return 0;
+    switch (uMsg)
+    {
+        case BFFM_INITIALIZED:
+            SendMessage(hWnd, BFFM_SETSELECTION, (WPARAM)TRUE, lpData);
+            break;
+    }
+    return 0;
 }
