c157a701dec453a01d35feaf443a7e3c3452f6f9
Lioncash <mathew1800@gmail.com>
2015-09-27 19:43:10 -0400

    BitUtils: Add swapping functions

--- Utils/BitUtils.cpp
+++ Utils/BitUtils.cpp
@@ -5,6 +5,39 @@
 namespace BitUtils
 {
 
+unsigned short Swap16(unsigned short value)
+{
+#ifdef _MSC_VER
+	return _byteswap_ushort(value);
+#elif __linux__
+	return __builtin_bswap16(value);
+#else
+	return (value << 8) | (value >> 8);
+#endif
+}
+
+unsigned int Swap32(unsigned int value)
+{
+#ifdef _MSC_VER
+	return _byteswap_ulong(value);
+#elif __linux__
+	return __builtin_bswap32(value);
+#else
+	return (Swap16(data) << 16) | Swap16(data >> 16);
+#endif
+}
+
+unsigned long long Swap64(unsigned long long value)
+{
+#ifdef _MSC_VER
+	return _byteswap_uint64(value);
+#elif __linux__
+	return __builtin_bswap64(value);
+#else
+	return (static_cast<unsigned long long>(Swap32(value)) << 32) | Swap32(value >> 32);
+#endif
+}
+
 unsigned int RotateLeft(const unsigned int value, int shift)
 {
 	if ((shift &= sizeof(value) * CHAR_BIT - 1) == 0)
--- Utils/BitUtils.h
+++ Utils/BitUtils.h
@@ -3,6 +3,10 @@
 namespace BitUtils
 {
 
+unsigned short Swap16(unsigned short value);
+unsigned int Swap32(unsigned int value);
+unsigned long long Swap64(unsigned long long value);
+
 unsigned int RotateLeft(unsigned int value, int shift);
 unsigned int RotateRight(unsigned int value, int shift);
 
