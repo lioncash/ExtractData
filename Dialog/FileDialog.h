a88deccfa1cbb9f1ea3d3fe4dc5cb43d08a7af71
Lioncash <mathew1800@gmail.com>
2015-09-28 02:13:57 -0400

    FileDialog: Remove unimplemented functions

--- Dialog/FileDialog.h
+++ Dialog/FileDialog.h
@@ -4,5 +4,4 @@ class CFileDialog
 {
 public:
 	static BOOL DoModal(HWND hWnd, LPTSTR pFileNames, LPTSTR LastReadFileDir);
-	static LRESULT CALLBACK BrowseCallBackProc(HWND hWnd, UINT uMsg, LPARAM lParam, LPARAM lpData);
 };
