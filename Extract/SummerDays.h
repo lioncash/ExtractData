0eb0e1f4fc64866e9f0a6028727668a2b25025eb
Lioncash <mathew1800@gmail.com>
2016-10-27 05:14:06 -0400

    SummerDays: Make non-interface functions private

--- Extract/SummerDays.h
+++ Extract/SummerDays.h
@@ -5,10 +5,6 @@ class CSummerDays final : public CExtractBase
 public:
 	BOOL Mount(CArcFile* pclArc) override;
 
-	int _sub(CArcFile* pclArc, LPTSTR pcPath);
-	WORD _context_new(CArcFile* pclArc, WORD ui16Length);
-	int _context_add(WORD ui16Code);
-
 private:
 	struct TCONTEXT
 	{
@@ -16,6 +12,10 @@ private:
 		int iType;
 		WORD ui16Code;
 	};
+
+	int _sub(CArcFile* pclArc, LPTSTR pcPath);
+	WORD _context_new(CArcFile* pclArc, WORD ui16Length);
+	int _context_add(WORD ui16Code);
 
 	std::vector<TCONTEXT> m_tContextTable;
 	WORD m_ui16ContextCount;
