ef60940793f531fe166578028af3371670d2200b
Lioncash <mathew1800@gmail.com>
2016-10-28 19:22:10 -0400

    UpDown: In-class initialize variables

--- Ctrl/UpDown.h
+++ Ctrl/UpDown.h
@@ -6,8 +6,8 @@ public:
 	HWND Create(HWND hWnd, HWND hBuddy, int nPos, UINT ID, int nUpper, int nLower, int x = 0, int y = 0, int cx = 0, int cy = 0);
 
 private:
-	HWND      m_hWnd;
-	HINSTANCE m_hInst;
-	HWND      m_hCtrl;
-	UINT      m_ID;
+	HWND      m_hWnd = nullptr;
+	HINSTANCE m_hInst = nullptr;
+	HWND      m_hCtrl = nullptr;
+	UINT      m_ID = 0;
 };
