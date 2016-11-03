38a1376b563492d5c1f0ddf3f49dddb6e2bb82ed
Lioncash <mathew1800@gmail.com>
2016-11-03 18:43:46 -0400

    TrH: Drop windows typedefs

--- Extract/TrH.h
+++ Extract/TrH.h
@@ -12,18 +12,18 @@ private:
 	// PX Format
 	struct PXHed
 	{
-		BYTE    pxID[4];        // "fPX "
-		DWORD   FileSize;       // File Size
-		BYTE    CtrkID[4];      // "cTRK"
-		DWORD   DataSize;       // Number of bytes of waveform data
-		DWORD   dummy1;         // Unknown
-		DWORD   dummy2;         // Unknown
-		DWORD   dummy3;         // Unknown
-		DWORD   freq;           // Sampling Rate
-		WORD    dummy4;         // Unknown
-		WORD    channels;       // Number of channels
-		DWORD   ChunkByte;      // Numer of bytes per chunk
-		WORD    bits;           // Number of bits per sample(bit/sample)
-		WORD    FormatID;       // Format ID
+		u8  px_id[4];       // "fPX "
+		u32 file_size;      // File Size
+		u8  ctrk_id[4];     // "cTRK"
+		u32 data_size;      // Number of bytes of waveform data
+		u32 dummy1;         // Unknown
+		u32 dummy2;         // Unknown
+		u32 dummy3;         // Unknown
+		u32 freq;           // Sampling Rate
+		u16 dummy4;         // Unknown
+		u16 channels;       // Number of channels
+		u32 chunk_byte;     // Numer of bytes per chunk
+		u16 bits;           // Number of bits per sample(bit/sample)
+		u16 format_id;      // Format ID
 	};
 };
