7ec95d3762359e2236a40fdd18d808f503709b33
Lioncash <mathew1800@gmail.com>
2016-11-17 06:03:36 -0500

    Ykc: Get rid of some Windows typedefs

--- Extract/Ykc.cpp
+++ Extract/Ykc.cpp
@@ -7,31 +7,31 @@ bool CYkc::Mount(CArcFile* archive)
 		return false;
 
 	// Get the index size and the offset to the index
-	DWORD index_offset;
-	DWORD index_size;
+	u32 index_offset;
+	u32 index_size;
 	archive->SeekHed(0x10);
-	archive->Read(&index_offset, 4);
-	archive->Read(&index_size, 4);
+	archive->ReadU32(&index_offset);
+	archive->ReadU32(&index_size);
 
 	// Get the index
-	YCMemory<BYTE> index(index_size);
+	std::vector<u8> index(index_size);
 	archive->SeekHed(index_offset);
-	archive->Read(&index[0], index_size);
+	archive->Read(index.data(), index.size());
 
 	// Get the offset of the filename, and the offset to the index file name
-	const DWORD file_name_index_offset = *(LPDWORD)&index[0];
-	const DWORD file_name_index_size = index_offset - file_name_index_offset;
+	const u32 file_name_index_offset = *(u32*)&index[0];
+	const u32 file_name_index_size = index_offset - file_name_index_offset;
 
 	// Get the filename index
-	YCMemory<BYTE> file_name_index(file_name_index_size);
+	std::vector<u8> file_name_index(file_name_index_size);
 	archive->SeekHed(file_name_index_offset);
-	archive->Read(&file_name_index[0], file_name_index_size);
+	archive->Read(file_name_index.data(), file_name_index.size());
 
 	// Get file information
-	for (DWORD i = 0, j = 0; i < index_size; i += 20)
+	for (u32 i = 0, j = 0; i < index_size; i += 20)
 	{
 		// Get the length of the filename
-		const DWORD file_name_length = *(LPDWORD)&index[i + 4];
+		const u32 file_name_length = *(u32*)&index[i + 4];
 
 		// Get the filename
 		TCHAR file_name[_MAX_FNAME];
@@ -41,8 +41,8 @@ bool CYkc::Mount(CArcFile* archive)
 		// Get file information
 		SFileInfo file_info;
 		file_info.name = file_name;
-		file_info.start = *(LPDWORD)&index[i + 8];
-		file_info.sizeCmp = *(LPDWORD)&index[i + 12];
+		file_info.start = *(u32*)&index[i + 8];
+		file_info.sizeCmp = *(u32*)&index[i + 12];
 		file_info.sizeOrg = file_info.sizeCmp;
 		file_info.end = file_info.start + file_info.sizeCmp;
 
@@ -73,31 +73,31 @@ bool CYkc::DecodeYKS(CArcFile* archive)
 		return false;
 
 	// Read the YKS file
-	YCMemory<BYTE> src(file_info->sizeCmp);
-	archive->Read(&src[0], file_info->sizeCmp);
+	std::vector<u8> src(file_info->sizeCmp);
+	archive->Read(src.data(), src.size());
 
-	if (memcmp(&src[0], "YKS001", 6) == 0)
+	if (memcmp(src.data(), "YKS001", 6) == 0)
 	{
 		// YKS001
 
 		// Get the offset of the text portion
-		const DWORD text_offset = *(LPDWORD)&src[0x20];
+		const u32 text_offset = *(u32*)&src[0x20];
 
 		// Decode the text portion
-		for (size_t i = text_offset; i < file_info->sizeCmp; i++)
+		for (size_t i = text_offset; i < src.size(); i++)
 		{
 			src[i] ^= 0xAA;
 		}
 
 		// Output
 		archive->OpenScriptFile();
-		archive->WriteFile(&src[0], file_info->sizeCmp);
+		archive->WriteFile(src.data(), src.size());
 	}
 	else
 	{
 		// Other
 		archive->OpenFile();
-		archive->WriteFile(&src[0], file_info->sizeCmp);
+		archive->WriteFile(src.data(), src.size());
 	}
 
 	return true;
@@ -111,10 +111,10 @@ bool CYkc::DecodeYKG(CArcFile* archive)
 		return false;
 
 	// Read the YKG file
-	YCMemory<BYTE> src(file_info->sizeCmp);
-	archive->Read(&src[0], file_info->sizeCmp);
+	std::vector<u8> src(file_info->sizeCmp);
+	archive->Read(src.data(), src.size());
 
-	if (memcmp(&src[0], "YKG000", 6) == 0)
+	if (memcmp(src.data(), "YKG000", 6) == 0)
 	{
 		// YKG000
 
@@ -123,13 +123,13 @@ bool CYkc::DecodeYKG(CArcFile* archive)
 
 		// Output
 		archive->OpenFile(_T(".png"));
-		archive->WriteFile(&src[0x40], file_info->sizeCmp - 0x40);
+		archive->WriteFile(&src[0x40], src.size() - 0x40);
 	}
 	else
 	{
 		// Other
 		archive->OpenFile();
-		archive->WriteFile(&src[0], file_info->sizeCmp);
+		archive->WriteFile(src.data(), src.size());
 	}
 
 	return true;
