011eedb3a9243d0e255ffeba744bcc9d34858708
Lioncash <mathew1800@gmail.com>
2016-11-20 04:30:07 -0500

    TokiPaku: Use reinterpret_cast instead of C casts

--- Extract/krkr/TokiPaku.cpp
+++ Extract/krkr/TokiPaku.cpp
@@ -57,7 +57,7 @@ size_t CTokiPaku::OnDecrypt(u8* target, size_t target_size, size_t offset, u32 d
 {
 	for (size_t i = 0; i < target_size; i += 4)
 	{
-		*(u32*)&target[i] ^= decrypt_key;
+		*reinterpret_cast<u32*>(&target[i]) ^= decrypt_key;
 	}
 
 	return target_size;
