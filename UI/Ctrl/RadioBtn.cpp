6b3cea2016da14ba28a3e7389c24642394a90851
Lioncash <mathew1800@gmail.com>
2016-11-27 22:03:40 -0500

    RadioBtn: Move function definitions in the header to the cpp file

--- UI/Ctrl/RadioBtn.cpp
+++ UI/Ctrl/RadioBtn.cpp
@@ -10,3 +10,34 @@ HWND CRadioBtn::Create(HWND hWnd, LPCTSTR lpCaption, UINT ID, int x, int y, int 
 	m_IDs.push_back(ID);
 	return hRadio;
 }
+
+void CRadioBtn::Close()
+{
+	m_hRadios.clear();
+	m_IDs.clear();
+}
+
+void CRadioBtn::SetCheck(BOOL flag)
+{
+	Button_SetCheck(GetCtrlHandle(), flag);
+}
+
+void CRadioBtn::SetCheck(DWORD num, BOOL flag)
+{
+	Button_SetCheck(m_hRadios[num], flag);
+}
+
+BOOL CRadioBtn::GetCheck() const
+{
+	return Button_GetCheck(GetCtrlHandle());
+}
+
+BOOL CRadioBtn::GetCheck(DWORD num) const
+{
+	return Button_GetCheck(m_hRadios[num]);
+}
+
+UINT CRadioBtn::GetID(DWORD num) const
+{
+	return (num >= m_IDs.size()) ? 0 : m_IDs[num];
+}
--- UI/Ctrl/RadioBtn.h
+++ UI/Ctrl/RadioBtn.h
@@ -7,12 +7,12 @@ class CRadioBtn : public CCtrlBase
 public:
 	HWND Create(HWND hWnd, LPCTSTR lpCaption, UINT uID, int x = 0, int y = 0, int cx = 0, int cy = 0) override;
 
-	void Close()                        { m_hRadios.clear(); m_IDs.clear(); }
-	void SetCheck(BOOL flag)            { Button_SetCheck(GetCtrlHandle(), flag); }
-	void SetCheck(DWORD num, BOOL flag) { Button_SetCheck(m_hRadios[num], flag); }
-	BOOL GetCheck()               const { return Button_GetCheck(GetCtrlHandle()); }
-	BOOL GetCheck(DWORD num)      const { return Button_GetCheck(m_hRadios[num]); }
-	UINT GetID(DWORD num)         const { return (num >= m_IDs.size()) ? 0 : m_IDs[num]; }
+	void Close();
+	void SetCheck(BOOL flag);
+	void SetCheck(DWORD num, BOOL flag);
+	BOOL GetCheck()          const;
+	BOOL GetCheck(DWORD num) const;
+	UINT GetID(DWORD num)    const;
 
 private:
 	std::vector<HWND> m_hRadios;
