4e26230512049b760df7f4faf65f017a0e820f36
Lioncash <mathew1800@gmail.com>
2016-11-27 21:53:29 -0500

    CheckBox: Move implementation in the header to the cpp file

--- UI/Ctrl/CheckBox.cpp
+++ UI/Ctrl/CheckBox.cpp
@@ -6,3 +6,13 @@ HWND CCheckBox::Create(HWND hWnd, LPCTSTR lpCaption, UINT ID, int x, int y, int 
 	Init(hWnd, ID);
 	return CreateCtrl(_T("BUTTON"), BS_AUTOCHECKBOX | WS_TABSTOP, lpCaption, x, y, cx, cy);
 }
+
+void CCheckBox::SetCheck(BOOL flag)
+{
+	Button_SetCheck(GetCtrlHandle(), flag);
+}
+
+BOOL CCheckBox::GetCheck() const
+{
+	return Button_GetCheck(GetCtrlHandle());
+}
--- UI/Ctrl/CheckBox.h
+++ UI/Ctrl/CheckBox.h
@@ -7,13 +7,6 @@ class CCheckBox : public CCtrlBase
 public:
 	HWND Create(HWND hWnd, LPCTSTR lpCaption, UINT uID, int x = 0, int y = 0, int cx = 0, int cy = 0) override;
 
-	void SetCheck(BOOL flag)
-	{
-		Button_SetCheck(GetCtrlHandle(), flag);
-	}
-
-	BOOL GetCheck() const
-	{
-		return Button_GetCheck(GetCtrlHandle());
-	}
+	void SetCheck(BOOL flag);
+	BOOL GetCheck() const;
 };
