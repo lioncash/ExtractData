abe4e2c470518231aebf1538e673a77493bfdf80
Lioncash <mathew1800@gmail.com>
2017-09-23 20:53:13 -0400

    ToolBar: Replace deprecated CreateToolbarEx() call with CreateWindowEx()

--- UI/Ctrl/ToolBar.cpp
+++ UI/Ctrl/ToolBar.cpp
@@ -19,30 +19,39 @@ HWND CToolBar::Create(HWND window, LPCTBBUTTON tool_bar_button, UINT bitmap_id, 
 {
 	Init(window);
 
+	m_tool_bar = CreateWindowEx(
+		0,                                   // Extended window style
+		TOOLBARCLASSNAME,                    // Class name
+		nullptr,                             // Window name
+		WS_CHILD | CCS_NORESIZE | TBSTYLE_WRAPABLE | TBSTYLE_TOOLTIPS, // Window style
+		0,                                   // Initial horizontal position
+		0,                                   // Initial vertical position
+		0,                                   // Window width (device units)
+		0,                                   // Window height (device units)
+		window,                              // Window parent
+		reinterpret_cast<HMENU>(ID_TOOLBAR), // Menu handle
+		m_inst,                              // Module instance
+		nullptr                              // WM_CREATE data
+	);
+
+	// Set generic toolbar properties
+	SendMessage(m_tool_bar, TB_BUTTONSTRUCTSIZE, static_cast<WPARAM>(sizeof(TBBUTTON)), 0);
+	SendMessage(m_tool_bar, TB_SETBITMAPSIZE, 0, MAKELPARAM(icon_cx, 16));
+
+	// Set the bitmaps.
 	HBITMAP bitmap = static_cast<HBITMAP>(LoadImage(m_inst, MAKEINTRESOURCE(bitmap_id), IMAGE_BITMAP, 0, 0, LR_LOADMAP3DCOLORS | LR_LOADTRANSPARENT));
+	TBADDBITMAP bitmap_info{nullptr, reinterpret_cast<UINT_PTR>(bitmap)};
+	SendMessage(m_tool_bar, TB_ADDBITMAP, ctIcon, reinterpret_cast<LPARAM>(&bitmap_info));
 
-	HWND tool_bar = CreateToolbarEx(
-		m_window,
-		WS_CHILD | WS_VISIBLE | CCS_NORESIZE | TBSTYLE_TOOLTIPS,
-		ID_TOOLBAR,
-		ctIcon,     // Number of images
-		nullptr,
-		reinterpret_cast<UINT_PTR>(bitmap),
-		tool_bar_button,
-		ctIcon,     // Number of buttons
-		0,          // Width of the button
-		0,          // Height of the button
-		icon_cx,    // Width of the image, 
-		16,         // Height of the image
-		sizeof(*tool_bar_button));
-
-	m_tool_bar = tool_bar;
+	// Add buttons and resize to fit
+	SendMessage(m_tool_bar, TB_ADDBUTTONS, ctIcon, reinterpret_cast<LPARAM>(tool_bar_button));
+	SendMessage(m_tool_bar, TB_AUTOSIZE, 0, 0);
 
 	// Flat
-	LONG_PTR tool_type = GetWindowLongPtr(tool_bar, GWL_STYLE) | TBSTYLE_FLAT;
-	SetWindowLongPtr(tool_bar, GWL_STYLE, tool_type);
-	InvalidateRect(tool_bar, nullptr, TRUE);
-	SendMessage(tool_bar, TB_SETEXTENDEDSTYLE, 0, static_cast<LPARAM>(TBSTYLE_EX_DRAWDDARROWS));
+	LONG_PTR tool_type = GetWindowLongPtr(m_tool_bar, GWL_STYLE) | TBSTYLE_FLAT;
+	SetWindowLongPtr(m_tool_bar, GWL_STYLE, tool_type);
+	InvalidateRect(m_tool_bar, nullptr, TRUE);
+	SendMessage(m_tool_bar, TB_SETEXTENDEDSTYLE, 0, static_cast<LPARAM>(TBSTYLE_EX_DRAWDDARROWS));
 
 	HWND tool_tip = CreateWindowEx(0, // Extended window style
 		TOOLTIPS_CLASS, // Class name
@@ -59,21 +68,22 @@ HWND CToolBar::Create(HWND window, LPCTBBUTTON tool_bar_button, UINT bitmap_id, 
 
 	TOOLINFO ti;
 	ti.cbSize = sizeof(TOOLINFO);
-	ti.hwnd = tool_bar;
+	ti.hwnd = m_tool_bar;
 	ti.hinst = m_inst;
 	ti.uFlags = TTF_SUBCLASS;
 
 	for (int i = 0; i < ctIcon; i++)
 	{
-		SendMessage(tool_bar, TB_GETITEMRECT, i, reinterpret_cast<LPARAM>(&ti.rect));
+		SendMessage(m_tool_bar, TB_GETITEMRECT, i, reinterpret_cast<LPARAM>(&ti.rect));
 		ti.uId = tool_bar_button[i].idCommand;
 		ti.lpszText = reinterpret_cast<LPTSTR>(tool_bar_button[i].idCommand);
 		SendMessage(tool_tip, TTM_ADDTOOL, 0, reinterpret_cast<LPARAM>(&ti));
 	}
 
-	SendMessage(tool_bar, TB_SETTOOLTIPS, reinterpret_cast<WPARAM>(tool_tip), 0);
+	SendMessage(m_tool_bar, TB_SETTOOLTIPS, reinterpret_cast<WPARAM>(tool_tip), 0);
 
-	return tool_bar;
+	ShowWindow(m_tool_bar, TRUE);
+	return m_tool_bar;
 }
 
 void CToolBar::SetWindowPos()
