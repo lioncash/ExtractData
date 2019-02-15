3db8a0372751006432e2f68f64548ecd4705c484
Lioncash <mathew1800@gmail.com>
2019-02-15 15:56:07 -0500

    BitUtils: Name parameters of the Swap* functions
    Makes it a little more consistent.

--- Utils/BitUtils.h
+++ Utils/BitUtils.h
@@ -2,9 +2,9 @@
 
 namespace BitUtils
 {
-u16 Swap16(u16);
-u32 Swap32(u32);
-u64 Swap64(u64);
+u16 Swap16(u16 value);
+u32 Swap32(u32 value);
+u64 Swap64(u64 value);
 
 u32 RotateLeft(u32 value, int shift);
 u32 RotateRight(u32 value, int shift);
