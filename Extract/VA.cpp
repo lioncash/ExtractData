2ca5b858261f43f84dfeb40d6f0f688599c80ec0
Lioncash <mathew1800@gmail.com>
2019-02-15 20:22:57 -0500

    VA: Move implementation detail structs to the cpp file

--- Extract/VA.cpp
+++ Extract/VA.cpp
@@ -4,6 +4,34 @@
 #include "ArcFile.h"
 #include "Sound/Wav.h"
 
+namespace
+{
+// NWA Format
+struct NWAHeader
+{
+	u16   channels;             // Number of channels
+	u16   bits;                 // Number of bits per sample (bit/sample)
+	u32   freq;                 // Sampling Rate
+	u32   compression_level;    // Compression Level
+	u32   dummy1;               // Unknown
+	u32   blocks;               // Number of blocks
+	u32   data_size;            // Number of bytes of waveform data after decompression
+	u32   compressed_file_size; // Compressed file size
+	u32   samples;              // Number of decompressed data
+	u32   block_size;           // The number of units of data samples of a block when you expand the data
+	u32   rest_size;            // Number of sample units of data when you expand the last block
+	u32   dummy2;               // Unknown
+};
+
+// NWK Index Header
+struct IndexNwk
+{
+	u32 size;
+	u32 start;
+	u32 dummy;
+};
+} // Anonymous namespace
+
 bool CVA::Mount(CArcFile* archive)
 {
 	if (MountNwa(archive))
--- Extract/VA.h
+++ Extract/VA.h
@@ -9,31 +9,6 @@ public:
 	bool Decode(CArcFile* archive) override;
 
 private:
-	// NWA Format
-	struct NWAHeader
-	{
-		u16   channels;             // Number of channels
-		u16   bits;                 // Number of bits per sample (bit/sample)
-		u32   freq;                 // Sampling Rate
-		u32   compression_level;    // Compression Level
-		u32   dummy1;               // Unknown
-		u32   blocks;               // Number of blocks
-		u32   data_size;            // Number of bytes of waveform data after decompression
-		u32   compressed_file_size; // Compressed file size
-		u32   samples;              // Number of decompressed data
-		u32   block_size;           // The number of units of data samples of a block when you expand the data
-		u32   rest_size;            // Number of sample units of data when you expand the last block
-		u32   dummy2;               // Unknown
-	};
-
-	// NWK Index Header
-	struct IndexNwk
-	{
-		u32 size;
-		u32 start;
-		u32 dummy;
-	};
-
 	bool MountNwa(CArcFile* archive);
 	bool MountNwk(CArcFile* archive);
 	bool MountOvk(CArcFile* archive);
