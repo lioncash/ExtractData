9766f238225d5a0e665f5332247d484f1d1f3984
Lioncash <mathew1800@gmail.com>
2019-02-15 20:30:29 -0500

    Navel: Move implementation detail struct to the cpp file

--- Extract/Navel.cpp
+++ Extract/Navel.cpp
@@ -4,6 +4,27 @@
 #include "ArcFile.h"
 #include "Sound/Wav.h"
 
+namespace
+{
+// WPD Format
+struct FormatWPD
+{
+  u8  id[4];       // " DPW"
+  u32 type;        // Unknown
+  u32 dummy2;      // Unknown
+  u32 chunk_byte;  // Number of bytes per chunk
+  u32 freq;        // Sampling rate
+  u32 data_size;   // Data Size
+  u16 format_id;   // Format ID
+  u16 channels;    // Number of channels
+  u32 freq2;       // Sampling rate (for some reason there's two)
+  u32 bps;         // Data Rate (Byte/sec)
+  u16 block_size;  // Block Size (Byte/sample * Number of channels)
+  u16 bits;        // Number of bits per sample (bit/sample)
+  u32 dummy3;      // Unknown
+};
+} // Anonymous namespace
+
 // Function that gets file information from Navel's .pac files
 bool CNavel::Mount(CArcFile* archive)
 {
--- Extract/Navel.h
+++ Extract/Navel.h
@@ -9,24 +9,6 @@ public:
 	bool Decode(CArcFile* archive) override;
 
 private:
-	// WPD Format
-	struct FormatWPD
-	{
-		u8  id[4];       // " DPW"
-		u32 type;        // Unknown
-		u32 dummy2;      // Unknown
-		u32 chunk_byte;  // Number of bytes per chunk
-		u32 freq;        // Sampling rate
-		u32 data_size;   // Data Size
-		u16 format_id;   // Format ID
-		u16 channels;    // Number of channels
-		u32 freq2;       // Sampling rate (for some reason there's two)
-		u32 bps;         // Data Rate (Byte/sec)
-		u16 block_size;  // Block Size (Byte/sample * Number of channels)
-		u16 bits;        // Number of bits per sample (bit/sample)
-		u32 dummy3;      // Unknown
-	};
-
 	bool MountPac(CArcFile* archive);
 	bool MountWpd(CArcFile* archive);
 };
